2954bb3dfbb963817a4369acd6be8762e46b2fc2
enggsuraj <suraj@Zibrans-MacBook-Pro.local>
2025-03-15 00:44:37 +0530

    fix/mobile-layout

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -5,7 +5,7 @@ function Card({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="card"
       className={cn(
-        "bg-card text-card-foreground flex flex-col gap-2 rounded-xl border py-6 shadow-sm min-w-[450px] sm:min-w-[400px]",
+        "bg-card text-card-foreground flex flex-col gap-2 rounded-xl border py-6 shadow-sm lg:min-w-[450px] sm:min-w-[400px]",
         className
       )}
       {...props}

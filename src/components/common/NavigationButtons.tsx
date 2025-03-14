7d7a1c3c4969590e24f53fe8d9bf93f3d95a1712
enggsuraj <suraj@Zibrans-MacBook-Pro.local>
2025-03-15 01:00:10 +0530

    fix/mobile-text-message-visibility

--- src/components/common/NavigationButtons.tsx
+++ src/components/common/NavigationButtons.tsx
@@ -22,15 +22,17 @@ export default function NavigationButtons() {
 
   return (
     <div className="grid grid-cols-2 gap-4 md:p-6 md:flex md:justify-center md:gap-4 md:flex-wrap">
-      {routes.map((route) => (
+      {routes.map((route, index) => (
         <button
-          key={route.path}
+          key={index}
           onClick={() => handleButtonClick(route.path)}
           className={`px-4 py-2 border border-black text-black text-sm rounded-sm cursor-pointer transition ${
             activePath === route.path ? "bg-black text-white" : "bg-white"
           }`}
         >
-          <Label className="cursor-pointer">{route.label}</Label>
+          <Label className="cursor-pointer flex justify-center">
+            {route.label}
+          </Label>
         </button>
       ))}
     </div>

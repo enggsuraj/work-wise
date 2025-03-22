b9f6cc8682b77f663c6e3835f56ee8c696d7f3e2
enggsuraj <info.blogtheorem@gmail.com>
2025-03-22 11:27:35 +0530

    🐛 Fix/build-error

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,13 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
-        source: "https://workwisetool.vercel.app/:path*",
-        destination: "https://www.workwisetool.vercel.app/:path*",
+        source: "/",
+        destination: "/notice-period",
         permanent: true,
       },
       {
-        source: "/",
-        destination: "/notice-period",
+        source: "/:path*/",
+        destination: "/:path*",
         permanent: true,
       },
       {

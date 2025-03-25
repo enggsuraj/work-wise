6d9b2e577a52d67043531b205458ff13551b7ef6
enggsuraj <info.blogtheorem@gmail.com>
2025-03-25 23:53:23 +0530

    🐛 Fix/text-message

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-gray-900 text-white p-3 mt-auto flex justify-center items-center">
       <p className="text-[8pt]">
-        © 2025 WorkWise. Built with 🖤 and ☕️ by{" "}
+        © 2025 WorkWiseTool Built with 🖤 and ☕️ by{" "}
         <a
           href="https://blogtheorem.vercel.app"
           target="_blank"

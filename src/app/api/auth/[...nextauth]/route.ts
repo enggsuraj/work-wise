fe507fa73b2eadc87b27f8039deea894f56ab170
enggsuraj <info.blogtheorem@gmail.com>
2026-04-19 03:57:10 +0530

    feat: update dependencies and enhance authentication setup with NextAuth integration

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,3 @@
-import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
+import { handlers } from "@/auth";
 
-const authOptions = {
-  providers: [
-    Google({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  pages: {
-    signIn: "/login",
-  },
-};
-
-const handler = NextAuth(authOptions);
-export const GET = handler;
-export const POST = handler;
+export const { GET, POST } = handlers;
